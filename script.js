1f691db4d24e288a95496e1031421fdeb1c79433
Nardin Lachowski <nardin@live.com>
2020-11-27 21:59:37 -0500

    uploading beginning of project

---
+++ script.js
@@ -0,0 +1,17 @@
+var currentDay = document.querySelector("#current-day");
+var currentTask = document.querySelector(".task");
+
+
+let date = moment().format('MMMM Do YYYY');
+let hour = moment().format('h'); // to test - currentDay.innerHTML = date + " " + hour;
+
+currentDay.innerHTML = date;
+
+
+let checkHour = '';
+
+for (let i = 0; i < 10; i++) {
+    
+}
+
+
